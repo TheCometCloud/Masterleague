4fa60370e8a47ae32b1c8ecb10c126a90cb5d539
Comet-Cloud <cometcloud7@gmail.com>
2018-11-04 02:49:33 -0500

    Allow the retrieval of teams after the first page

--- MasterLeague/MasterLeague/Retriever.cs
+++ MasterLeague/MasterLeague/Retriever.cs
@@ -11,39 +11,52 @@ namespace MasterLeague
 {
     public class Retriever
     {
-        static readonly string HEROES_URL = "https://api.masterleague.net/heroes/?format=json";
-        static readonly string MAPS_URL = "https://api.masterleague.net/maps/?format=json";
-        static readonly string TEAMS_URL = "https://api.masterleague.net/teams/?format=json";
-        static readonly string REGIONS_URL = "https://api.masterleague.net/regions/?format=json";
-        static readonly string PATCHES_URL = "https://api.masterleague.net/patches/?format=json";
-        static readonly string PLAYERS_URL = "https://api.masterleague.net/players/?format=json";
-        static readonly string TOURNAMENTS_URL = "https://api.masterleague.net/tournaments/?format=json";
-        static readonly string MATCHES_URL = "https://api.masterleague.net/matches/?format=json";
-        static readonly string CALENDAR_URL = "https://api.masterleague.net/calendar/?format=json";
+        static readonly string HEROES_URL = "https://api.masterleague.net/heroes/";
+        static readonly string MAPS_URL = "https://api.masterleague.net/maps/";
+        static readonly string TEAMS_URL = "https://api.masterleague.net/teams/";
+        static readonly string REGIONS_URL = "https://api.masterleague.net/regions/";
+        static readonly string PATCHES_URL = "https://api.masterleague.net/patches/";
+        static readonly string PLAYERS_URL = "https://api.masterleague.net/players/";
+        static readonly string TOURNAMENTS_URL = "https://api.masterleague.net/tournaments/";
+        static readonly string MATCHES_URL = "https://api.masterleague.net/matches/";
+        static readonly string CALENDAR_URL = "https://api.masterleague.net/calendar/";
+
+        static readonly string JSON_FORMAT = "format=json";
 
         public static string GetAllHeroes()
         {
-            string Json = new WebClient().DownloadString(HEROES_URL);
+            string Json = new WebClient().DownloadString(HEROES_URL + "?" + JSON_FORMAT);
             return Json;
         }
 
         public static string GetAllMaps()
         {
-            string Json = new WebClient().DownloadString(MAPS_URL);
+            string Json = new WebClient().DownloadString(MAPS_URL + "?" + JSON_FORMAT);
             return Json;
         }
 
         public static IList<Team> GetAllTeams()
         {
             IList<Team> teams = new List<Team>();
-            string Json = new WebClient().DownloadString(TEAMS_URL);
-
-            dynamic tmp = JsonConvert.DeserializeObject(Json);
-            foreach (dynamic result in tmp.results)
+            
+            string Json = new WebClient().DownloadString(TEAMS_URL + "?" + JSON_FORMAT);
+            dynamic tmp;
+            do
             {
-                Team team = JsonConvert.DeserializeObject<Team>(result.ToString());
-                teams.Add(team);
-            }
+                tmp = JsonConvert.DeserializeObject(Json);
+
+                foreach (dynamic result in tmp.results)
+                {
+                    Team team = JsonConvert.DeserializeObject<Team>(result.ToString());
+                    teams.Add(team);
+                }
+
+                if (tmp.next != null)
+                {
+                    Json = new WebClient().DownloadString(tmp.next.ToString() + "&" + JSON_FORMAT);
+                }
+            } while (tmp.next != null);
+
             return teams;
         }
     }
